6e8efa6eeae759bf6d8074058ae5f1f26b2306d6
Manuel Kießling <manuel@kiessling.net>
2026-02-22 17:33:30 +0100

    Productbuilder workspace init

--- .productbuilder/workspace-init.sh
+++ .productbuilder/workspace-init.sh
@@ -28,12 +28,11 @@ ETFS_PROJECT_NAME="${ETFS_PROJECT_NAME}"
 EOF
 
 docker compose up --build -d
-
 docker compose exec -T app composer install --no-interaction
-docker compose exec -T app mise trust
-docker compose exec -T app mise install
-docker compose exec -T app mise exec node -- npm install --no-save
-docker compose exec -T app php bin/console doctrine:database:create --if-not-exists
-docker compose exec -T app php bin/console doctrine:migrations:migrate --no-interaction
-docker compose exec -T app mise exec node -- php bin/console tailwind:build
-docker compose exec -T app mise exec node -- php bin/console typescript:build
+
+mise run in-app-container mise trust
+mise run in-app-container mise install
+mise run npm install --no-save
+mise run console doctrine:database:create --if-not-exists
+mise run console doctrine:migrations:migrate --no-interaction
+mise run frontend
